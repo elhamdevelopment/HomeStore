6abf870def73483901ad233beda70288844e299b
reza yazdanpanah <reza.ydpn@gmail.com>
2019-04-20 00:06:52 +0430

    add controller

---
+++ app/Http/Controllers/Admin/SettingController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HomeStore\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use HomeStore\Http\Controllers\Controller;
+
+class SettingController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/User/UserWalletController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HomeStore\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use HomeStore\Http\Controllers\Controller;
+
+class UserWalletController extends Controller
+{
+    //
+}
